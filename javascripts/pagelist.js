31eb50739ed167d5824108c95b9e4feea302044d
Joachim Fornallaz <fjoachim@gmail.com>
2010-08-08 22:53:13 +0200

    First pages commit

---
+++ javascripts/pagelist.js
@@ -0,0 +1,62 @@
+/*
+	PageList
+
+	Author:
+		Joachim Fornallaz, http://github.com/fjoachim
+
+	History:
+		Sometime in 2006  - v1.0
+*/
+
+var PageList = Class.create();
+
+PageList.lists = [];
+
+PageList.add = function(container, tagName) {
+	var list = new PageList(container, tagName);
+}
+
+PageList.prototype = {
+	initialize: function(container, tagName) {
+		var self = this;
+		this.container = $(container);
+		this.container.onselectstart = function() { return false; };
+		this.items = $A(this.container.getElementsByTagName(tagName));
+		this.items.each( function(item) {
+			item.onclick = self.itemClicked.bindAsEventListener(self);
+		} );
+	},
+	
+	itemClicked: function(event) {
+		var item = Event.element(event);
+		var self = this;
+
+		if (event.shiftKey) {
+			firstSelection = -1;
+			lastSelection = -1;
+			
+			this.items.each( function(loopItem) {
+				if (item == loopItem) {
+					loopItem.className = 'selected';
+				};
+				
+				if (loopItem.className == 'selected') {
+					lastSelection = self.items.indexOf(loopItem);
+					if (firstSelection < 0) { 
+						firstSelection = lastSelection;
+					}
+				}
+			} );
+			
+			for (var i = firstSelection; i <= lastSelection; i++) {
+				this.items[i].className = 'selected';
+			}
+			
+		} else if (!event.metaKey) {
+			this.items.each( function(item) { item.className = 'normal'; } );
+			item.className = 'selected';
+		} else {
+			item.className = (item.className == 'selected') ? 'normal' : 'selected';
+		}
+	}
+}
